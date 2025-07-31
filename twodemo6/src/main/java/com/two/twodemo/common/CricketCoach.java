afc687db45c461415b682ddbcd7267bcf421bb20
Prakhyath Shri <prakhyath2004@gmail.com>
2025-07-31 23:18:30 +0530

    Lazy Initialization

---
+++ twodemo6/src/main/java/com/two/twodemo/common/CricketCoach.java
@@ -0,0 +1,17 @@
+package com.two.twodemo.common;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CricketCoach implements Coach {
+
+
+    public  CricketCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "practice fast bowling for 15 min";
+    }
+}
